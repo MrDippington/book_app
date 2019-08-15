3b5f3436e393b1e18b232aa146d761be86c91b33
ryan sanders <ryanpaul.smg@gmail.com>
2019-08-14 20:15:32 -0700

    connectivity confirmed

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
-"use strict";
+'use strict';
 
-require("dotenv").config();
+// require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const superagent = require("superagent");
 const PORT = process.env.PORT || 3000;
-const pg = require('pg');
+// const pg = require('pg');
 
 app.use(cors());
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+app.use(express.urlencoded({extended:true}));
+
+//routes
+app.get('/', (request, response) => {
+  //do something ejs-ey
+  response.render('pages/index');
+})
+
+
+
+
+app.listen(PORT, () => console.log(`server up on ${PORT}`));
+
